5507ccbc87ebab8b6bc99c850e4f0086c27b2ea2
Barshan <barshan.itman@gmail.com>
2025-08-02 01:02:22 +1000

    Added dropout layers to prevent overfitting

--- train.py
+++ train.py
@@ -24,24 +24,26 @@ else:
     EPOCHS = int(EPOCHS)
 
 class Net(nn.Module):
-	def __init__(self):
-			super().__init__()
-			self.conv1 = nn.Conv2d(3, 6, 5)
-			self.pool = nn.MaxPool2d(2, 2)
-			self.conv2 = nn.Conv2d(6, 16, 5)
-			self.fc1 = nn.Linear(44944, 120)
-			self.fc2 = nn.Linear(120, 84)
-			self.fc3 = nn.Linear(84, 53)
+    def __init__(self):
+            super().__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(44944, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 53)
+            self.dropout = nn.Dropout(p=0.5)
 
-	def forward(self, x):
-			x = self.pool(F.relu(self.conv1(x)))
-			x = self.pool(F.relu(self.conv2(x)))
-			x = torch.flatten(x, 1) # flatten all dimensions except batch
-			x = F.relu(self.fc1(x))
-			x = F.relu(self.fc2(x))
-			x = self.fc3(x)
-			return x
-
+    def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = torch.flatten(x, 1) # flatten all dimensions except batch
+            x = F.relu(self.fc1(x))
+            x = self.dropout(x)
+            x = F.relu(self.fc2(x))
+            x = self.dropout(x)
+            x = self.fc3(x)
+            return x
 
 if __name__ == "__main__":
     X,y = get_training_dataset() 
